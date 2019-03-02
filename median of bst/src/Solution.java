94a0896ca7c0814b47c64bf08adefaa36db582ce
Haodong Zheng <hz288@carr026.nb-labs.rutgers.edu>
2019-03-01 21:24:19 -0500

    finished

--- median of bst/src/Solution.java
+++ median of bst/src/Solution.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class TreeNode{
 	int val;
 	TreeNode left;
@@ -7,11 +8,42 @@ class TreeNode{
 	}
 }
 public class Solution {
+	private static List<Integer> ans = new LinkedList<>();
+	public static void ans (TreeNode node) {
+		mean(node);
+		
+		int len = ans.size();
+		if(len %2 == 0) {
+			System.out.println("ans is " + (ans.get(len/2) + ans.get(len/2-1))/2);
+		}else {
+			System.out.println("ans is " + (node.val+1));
+		}
+	}
+	public static void mean(TreeNode node) {
+		if(node == null)
+			return ;
+		mean(node.left);
+		ans.add(node.val);
+		mean(node.right);
+	}
 
 	public static void main(String[]args) {
 		TreeNode root = new TreeNode(6);
 		root.left = new TreeNode(3);
 		root.right = new TreeNode(8);
+		root.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(4);
+		root.right.left = new TreeNode(7);
+		root.right.right = new TreeNode(9);
 		
+		
+		
+		TreeNode t = new TreeNode(6);
+		t.left = new TreeNode(3);
+		t.left.left = new TreeNode(1);
+		t.left.right = new TreeNode(4);
+		t.right = new TreeNode(8);
+		t.right.left = new TreeNode(7);
+		ans(t);
 	}
 }
